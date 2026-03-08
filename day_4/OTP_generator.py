b0cc46f8f40c327a34aa69782c23fc7d24ad9ae1
pavankumarr25 <pavankumarduddi25@gmail.com>
2026-03-08 10:19:25 +0530

    Reorganized python practice files into day-wise folders

---
+++ day_4/OTP_generator.py
@@ -0,0 +1,5 @@
+import random
+
+password = random.randint(1000,9999)
+
+print("Your OTP is:", password)
---
+++ day_4/random_module.py
@@ -0,0 +1,10 @@
+import random
+
+# import random
+
+num = random.randint(1, 10) # gives an integer number
+
+
+num = random.random()    #gives a decimal number
+
+print(num)
